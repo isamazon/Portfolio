6befe6c99a01825bbed65d593c9a87be556d303b
Isaiah Mazon <37008425+isamazon@users.noreply.github.com>
2021-05-04 00:19:58 -0700

    Made adjustments to the responsiveness

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,11 +1,11 @@
 import { render } from "@testing-library/react";
 import React, { useState, Component } from "react";
-import { Container } from "reactstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import { ThemeProvider } from "styled-components";
 // Assets
 import video1 from "../assets/clouds.mp4";
 import video2 from "../assets/sunbg.mp4";
-
+import resume from "../assets/isaiahsresume.pdf";
 // Icons
 
 import { FaGithub, FaLinkedinIn, FaRegMoon, FaSun } from "react-icons/fa";
@@ -21,66 +21,96 @@ class Landing extends React.Component {
     this.state = {
       linkColor: "link",
       video: video1,
+      welcome: "welcome-h1",
+      devh1: "dev-h1",
+      lp2BG: "projects-fluid-cnt",
     };
   }
   render() {
     return (
-      <Container fluid className="fluid-cont">
-        <div className="mode-btn">
-          <div className="btn">
-            <WiDaySunny
-              onClick={() =>
-                this.setState({
-                  linkColor: "link-lighttheme",
-                  video: video2,
-                })
-              }
-              size="40px"
-              className="color-icon"
-            />
-            <h1 className="divider">/</h1>
-            <WiMoonWaxingCrescent1
-              onClick={() =>
-                this.setState({ linkColor: "link", video: video1 })
-              }
-              size="40px"
-              className="color-icon-2"
-            />
+      <div className="parent-cont">
+        <Container fluid className="fluid-cont">
+          <div className="mode-btn">
+            <div className="btn">
+              <WiDaySunny
+                onClick={() =>
+                  this.setState({
+                    linkColor: "link-lighttheme",
+                    video: video2,
+                    welcome: "welcome-h1-light",
+                    devh1: "dev-h1-light",
+                    lp2BG: "projects-fluid-cnt projects-lightBG",
+                  })
+                }
+                size="40px"
+                className="color-icon"
+              />
+              <h1 className="divider">/</h1>
+              <WiMoonWaxingCrescent1
+                onClick={() =>
+                  this.setState({
+                    linkColor: "link",
+                    video: video1,
+                    welcome: "welcome-h1",
+                    devh1: "dev-h1",
+                    lp2BG: "projects-fluid-cnt",
+                  })
+                }
+                size="40px"
+                className="color-icon-2"
+              />
+            </div>
           </div>
-        </div>
-        <div className="link-container">
-          <a
-            className={this.state.linkColor}
-            href="https://github.com/isamazon"
-          >
-            <FaGithub size="30px" className="github-icon" />
-          </a>
-          <a
-            className={this.state.linkColor}
-            href="https://github.com/isamazon"
-          >
-            <SiIndeed size="30px" className="github-icon" />
-          </a>
-          <a className={this.state.linkColor} href="">
-            <FaLinkedinIn size="30px" className="pdf" />
-          </a>
-          <a className={this.state.linkColor} href="">
-            <HiOutlineDocumentDownload size="35px" className="pdf" />
-          </a>
-        </div>
-        <video
-          className="video1"
-          autoPlay
-          muted
-          loop
-          src={this.state.video}
-        ></video>
-        <Container className="main-cont">
-          <h1 className="welcome-h1">WELCOME</h1>
-          <h1 className="name-h1">Im isaiah mazon,</h1>
-          <h1 className="dev-h1">FRONT END WEB DEV</h1>
+          <div className="link-container">
+            <a
+              className={this.state.linkColor}
+              href="https://github.com/isamazon"
+            >
+              <FaGithub size="30px" className="github-icon" />
+            </a>
+            <a
+              className={this.state.linkColor}
+              href="https://my.indeed.com/p/isaiahm-pujw7f2"
+            >
+              <SiIndeed size="30px" className="github-icon" />
+            </a>
+            <a
+              className={this.state.linkColor}
+              href="https://www.linkedin.com/in/isaiah-mazon-1786b8206/"
+            >
+              <FaLinkedinIn size="30px" className="pdf" />
+            </a>
+            <a
+              className={this.state.linkColor}
+              href={resume}
+              download="isaiah Mazon Resume"
+            >
+              <HiOutlineDocumentDownload size="35px" className="pdf" />
+            </a>
+          </div>
+          <video
+            className="video1"
+            src={this.state.video}
+            autoPlay
+            muted
+            loop
+          ></video>
+          <Container className="main-cont">
+            <h1 className={this.state.welcome}>WELCOME</h1>
+            <h1 className="name-h1">Im isaiah mazon,</h1>
+            <h1 className={this.state.devh1}>FRONT END WEB DEV</h1>
+          </Container>
         </Container>
-      </Container>
+        <Container fluid className={this.state.lp2BG}>
+          <Container className="projects-cnt">
+            <Row>
+              <Col>
+                <h1>ee</h1>
+              </Col>
+            </Row>
+          </Container>
+        </Container>
+      </div>
     );
   }
 }
